a91a3b35410feaa2e5b6d19e99e8e174f5bd8283
rmflight <r.flight79@gmail.com>
2023-02-09 15:36:54 -0500

    renaming functions, and deprecating old versions

--- R/raw_processing.R
+++ R/raw_processing.R
@@ -13,12 +13,27 @@ process_string_data = function(string_file){
   return(string_data)
 }
 
-#' process STRING ID files
+#' process raw STRING link data
 #'
-#' given a STRING ID file, generate a data.frame that can be used to map
+#' given a STRING links file, process it and save it for future use
+#'
+#' @param string_links the links file
+#' @export
+#' @return data.frame
+#'
+process_string_links = function(string_links){
+  stopifnot(file.exists(string_links))
+
+  string_ppi = read.table(string_links, header = TRUE, sep = " ", stringsAsFactors = FALSE)
+  return(string_ppi)
+}
+
+#' process STRING id files
+#'
+#' given a STRING id file, generate a data.frame that can be used to map
 #' various symbols
 #'
-#' @param string_file the id file
+#' @param string_file the alias file
 #' @export
 #' @return data.frame
 #'
@@ -26,6 +41,23 @@ process_string_id = function(string_file){
   stopifnot(file.exists(string_file))
 
   string_id = read.table(string_file, sep = "\t", header = FALSE, stringsAsFactors = FALSE, quote = "", fill = TRUE)
+  names(string_id) = c("string", "other", "type")
+  return(string_id)
+}
+
+#' process STRING alias files
+#'
+#' given a STRING alias file, generate a data.frame that can be used to map
+#' various symbols
+#'
+#' @param string_aliases the alias file
+#' @export
+#' @return data.frame
+#'
+process_string_aliases = function(string_aliases){
+  stopifnot(file.exists(string_aliases))
+
+  string_id = read.table(string_aliases, sep = "\t", header = FALSE, stringsAsFactors = FALSE, quote = "", fill = TRUE)
   names(string_id) = c("string", "other", "type")
   return(string_id)
 }
